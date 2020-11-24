0339730470a2c94f70509d43af94aa37f3e22f2f
russbjork <rbjork@gmail.com>
2020-11-24 07:59:56 +0000

    Built the initial function structure for programming assignment 3

---
+++ best.R
@@ -0,0 +1,8 @@
+best <- function(state,outcome){
+    ## Read outcome data
+  
+    ## Check state and outcome for validity
+  
+    ## Return hospital name in the state with lowest 30-day death rate
+  
+}
---
+++ rankall.R
@@ -0,0 +1,10 @@
+rankall <-  function(outcome, num = "best") {                                       
+    ## Read outcome data                                                    
+
+    ## For each state, find the hospital of the given rank                  
+
+    ## Return a data frame with the hospital names and the (abbreviated)    
+
+    ## state name                                                           
+
+} 
