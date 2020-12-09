de8c06c1337e5c9640acc9353af73d1461fcd02c
russbjork <rbjork@gmail.com>
2020-12-09 11:40:02 +0000

    Updated for limited data load

--- working.R
+++ working.R
@@ -3,6 +3,7 @@
 ##
 library(dplyr)
 library(ggplot2)
+library(ggpubr)
 library(knitr)
 opts_chunk$set(echo = TRUE)
 
@@ -16,31 +17,71 @@ download.file(url, temp)
 ##
 ## Read in raw data from URL
 ##
-rawdata <- read.csv(temp)
+# rawdata <- read.csv(temp)
+## Limited load to speed up development
+rawdata <- read.csv(temp, nrows=250000)
 head(rawdata)
 
 ##
-## Explore the raw data
+## Explore the raw data identify any potential issues
 ##
 str(rawdata)
 summary(rawdata)
 unique(rawdata$STATE)[1:52]
+## Create the Continental US vector
 states <- c("AL","AZ","AR","CA","CO","CT","DE","FL","GA","ID","IL","IN","IA","KS","KY","LA",
             "ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND", 
             "OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY")
 
 ##
-## Transform the data convert date attribute to valid Date - NEW DATASET
+## Transform the data limited to continental US only - NEW DATASET
+## Data filtered/grouped summary and calculations
 ##
-data <- rawdata %>% select(STATE,EVTYPE,FATALITIES,INJURIES) %>% filter(STATE %in% states) %>%
+datafatal <- rawdata %>% select(STATE,EVTYPE,FATALITIES,INJURIES) %>% filter(STATE %in% states) %>%
     select(EVTYPE,FATALITIES,INJURIES) %>% group_by(EVTYPE) %>% 
     summarize(FATALITIES=sum(FATALITIES), INJURIES=sum(INJURIES)) %>%
-    arrange(desc(FATALITIES),desc(INJURIES)) %>% filter(FATALITIES!=0)
-summary(data)
-str(data)
-data
+    arrange(desc(FATALITIES),desc(INJURIES)) %>% filter(FATALITIES!=0) %>% top_n(10)
+summary(datafatal)
+str(datafatal)
+datafatal
+
+datainjury <- rawdata %>% select(STATE,EVTYPE,FATALITIES,INJURIES) %>% filter(STATE %in% states) %>%
+  select(EVTYPE,FATALITIES,INJURIES) %>% group_by(EVTYPE) %>% 
+  summarize(FATALITIES=sum(FATALITIES), INJURIES=sum(INJURIES)) %>%
+  arrange(desc(INJURIES),desc(FATALITIES)) %>% filter(INJURIES!=0) %>% top_n(10)
+summary(datainjury)
+str(datainjury)
+datainjury
 
 ##
+## Graph and plot the transformed data US continental event data
+##
+# Basic scatter plot of fatalities by event type
+fplot <- ggplot(datafatal, aes(reorder(EVTYPE, -FATALITIES), y=FATALITIES)) +
+          geom_line(color="blue",group=1) +
+          geom_point(color="black",size=2) +
+          geom_text(aes(label=FATALITIES),vjust=0,hjust=0,size=3,nudge_x=0.2,nudge_y=0.2) +
+          labs(title="Top 10 Fatalities per Event Type",x="Event Type",y="Fatatilies") +
+          theme(plot.title.position='plot',plot.title=element_text(hjust=0.5)) +
+          theme(axis.text.x = element_text(angle=20,vjust=1,hjust=1))
+  
+# Basic scatter plot of injuries by event type
+iplot <- ggplot(datainjury, aes(reorder(EVTYPE, -INJURIES), y=INJURIES)) +
+          geom_line(color="blue",group=1) +
+          geom_point(color="black",size=2) +
+          geom_text(aes(label=INJURIES),vjust=0,hjust=0,size=3,nudge_x=0.2,nudge_y=0.2) +
+          labs(title="Top 10 Injuries per Event Type",x="Event Type",y="Injuries") +
+          theme(plot.title.position='plot',plot.title=element_text(hjust = 0.5)) +
+          theme(axis.text.x = element_text(angle=20,vjust=1,hjust=1))
+
+# Combine the two seperate plots
+ggarrange(fplot, iplot, ncol = 1, nrow = 2)
+
+# Output the plot to file
+dev.copy(png, file="plot1.png", height=640, width=720)
+dev.off()
+##
+## Transform the data limited to continental US only - NEW DATASET
 ## Data filtered/grouped summary and calculations
 ##
 
@@ -48,13 +89,7 @@ data
 ##
 ## Graph and plot the transformed data
 ##
-# Basic histogram of summarized steps by day
-#ggplot(dailysteps, aes(x=steps)) + 
-#  geom_histogram(binwidth=1000, color="blue", fill="white") +
-#  labs(title = "Histogram of Steps per day", x = "Steps", y = "Frequency") +
-#  theme(plot.title.position = 'plot',plot.title = element_text(hjust = 0.5))
-
-# Basic time series of mean steps per day
+# Basic series plot of fatalities and injuries by event type
 #ggplot(intvlsteps, aes(x=interval, y=steps)) +
 #  geom_line(color = "blue") +
 #  labs(title = "Time Series of Steps per Interval", x = "Interval", y = "Steps") +
